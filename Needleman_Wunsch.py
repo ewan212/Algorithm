a038e4d849354eb18ba5d71748b61e2bb106ab2d
evie wan <ewan@broadinstitute.org>
2022-06-08 15:26:36 -0400

    nw mod

--- Needleman_Wunsch.py
+++ Needleman_Wunsch.py
@@ -2,16 +2,15 @@
 # Global alignment
 
 from nis import match
-from turtle import up
+from tempfile import tempdir
+from turtle import setundobuffer, up
+from cv2 import ml_TrainData
 import numpy as np
 import itertools
 
-for i in range(-2,((len(ref)+1)*gap_cost),-2):
-    print(i)
-
-
 
 def score_matrix_v0(seq, ref, match_score, gap_cost):
+    '''first version'''
     # initialize matrix
     matrix = np.zeros((5,6))
     matrix[0,1:] = [i for i in range(-2,((len(ref)+1)*gap_cost),-2)]
@@ -35,15 +34,12 @@ def score_matrix_v0(seq, ref, match_score, gap_cost):
 
 
 
-# matrix = np.zeros((len(b) + 1, len(a) + 1))
-# matrix[0,1:] = [i for i in range(0, (len(a)*gap_cost),-2)]
-# matrix[0,1:] = [i for i in range(0,(len(b)*gap_cost),-2)]
 
 def score_matrix_v1(seq, ref, match_score, gap_cost):
-    '''more concise using itertools'''
+    '''using itertools and numpy'''
     matrix = np.zeros((len(seq) + 1, len(ref) + 1))
-    matrix[0,1:] = [i for i in range(-2,((len(ref)+1)*gap_cost),-2)]
-    matrix[1:,0] = [i for i in range(-2,((len(seq)+1)*gap_cost),-2)]        
+    matrix[0,:] = np.linspace(0, (len(ref)*gap_cost),len(ref)+1) 
+    matrix[:,0] = np.linspace(0, (len(seq)*gap_cost),len(seq)+1) 
     for i, j in itertools.product(range(1, matrix.shape[0]), range(1, matrix.shape[1])):
         match = matrix[i-1,j-1] + (match_score if seq[i-1] == ref[j-1] else - match_score)
         deletion = matrix[i-1,j] + gap_cost
@@ -58,6 +54,10 @@ seq = 'AGCT'
 ref = 'ATGCT'
 matrix = score_matrix_v1(seq, ref, match_score, gap_cost)
 
+h = matrix
+h_flip = np.flip(np.flip(matrix,0), 1)
+i_, j_ = np.unravel_index(h_flip.argmax(), h_flip.shape)
+i, j = np.subtract(h.shape, (i_ + 1, j_ + 1))
 
 
 
@@ -67,48 +67,74 @@ matrix = score_matrix_v1(seq, ref, match_score, gap_cost)
 # append letter to seq and ref
 # subtract index
 
+def needleman_wunsch(matrix):
+    # for indexing matrix
+    (m,n) = np.subtract(matrix.shape, (1,1))
+
+    # store alignment info
+    align_seq = ''
+    align_ref = ''
+
+    # scores and directions
+    diagonal = (1,1)
+    left = (0,1)
+    up = (1,0)
 
 
-(m,n) = np.subtract(matrix.shape, (1,1))
+    while m>0 and n>0:
+        m_temp = matrix[m-1:m+1,n-1:n+1]
+        # if sequences match, go back diagonally
+        if seq[m-1] == ref[n-1]: 
+            align_seq += seq[m-1]
+            align_ref += ref[n-1] 
+            (m, n) = np.subtract((m,n), diagonal)
 
-align_seq = ''
-align_ref = ''
+        else: 
+            # find max value neighbor 
+            if np.argsort(m_temp.flatten()[:-1])[-1] == 1:
+                align_ref += '-'
+                align_seq += ref[n-1]
+                (m,n) = np.subtract((m,n), up)
 
+            elif np.argsort(m_temp.flatten()[:-1])[-1] == 2:
+                align_seq += '_'
+                align_ref += ref[n-1]
+                (m,n) = np.subtract((m,n), left)
 
-diagonal = (1,1)
-left = (0,1)
-up = (1,0)
-
-while m>0 and n>0:
-    m_temp = matrix[m-1:m+1,n-1:n+1]
-    if seq[m-1] == ref[n-1]: 
-        align_seq += seq[m-1]
-        align_ref += ref[n-1] 
-        (m, n) = np.subtract((m,n), diagonal)
-
-
-    else: 
-        # max value neighbor 
-        if np.argsort(m_temp.flatten()[:-1])[-1] == 1:
-            align_ref += '-'
-            align_seq += ref[n-1]
-            (m,n) = np.subtract((m,n), up)
-
-        elif np.argsort(m_temp.flatten()[:-1])[-1] == 2:
-            
-            align_seq += '_'
-            align_ref += ref[n-1]
-            (m,n) = np.subtract((m,n), left)
-
-print(align_ref[::-1])
-print(align_seq[::-1])
-
-
-
-
+    return align_ref[::-1], align_seq[::-1]
     
 
+# base case: m,n==0
+max(m_temp.flatten()[:-1])
+back_max = np.where(m_temp == max(m_temp.flatten()[:-1]))
+(a,b) = (back_max[0][0], back_max[1][0])
+
+def traceback(m,n):
+    if m==0 or n==0:
+        return align_seq, align_ref 
+    else: 
+        if seq[m-1] == ref[n-1]: 
+            align_seq += seq[m-1]
+            align_ref += ref[n-1] 
+            (m, n) = np.subtract((m,n), diagonal)
+
+        else: 
+            m_temp = matrix[m-1:m+1,n-1:n+1]
+            # find max value neighbor 
+            back_max = np.where(m_temp == max(m_temp.flatten()[:-1]))
+            (a,b) = (back_max[0][0], back_max[1][0])
+
+    return traceback(m,n)
 
 
-        
+
+
+
+
+fib(n-1, n-2)
+
+
+
+
+
 
